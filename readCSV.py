d546765d57d05c0b9e612c3458eaf67b4a179a7d
nishantgupta91 <Nishant@Nishant-PC.mshome.net>
2015-06-03 22:19:56 +0200

    IAM Data filtered to get a Dataset of Handwritten pages of 10 different writers

---
+++ readCSV.py
@@ -0,0 +1,32 @@
+import csv
+import os
+import shutil
+
+fn = os.path.join(os.path.dirname(__file__), 'forms.csv')
+with open(fn) as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    writers = []
+    rows = []
+    for row in spamreader:
+        #print row
+        rows.append(row)
+        writers.append(row[1])
+        #print writers
+    countVar=1
+    writers_filtered = []
+    for i in range(1,1000):
+        if writers.count(str(i)) >= 10 and countVar<11:
+            writers_filtered.append(i)
+            #print " for i : %d, count is %d " % (i, writers.count(str(i)))
+            countVar = countVar+1
+    print writers_filtered
+    
+    for row in rows:
+        for i in range(0, len(writers_filtered)):
+            if row[1]==str(writers_filtered[i]):
+                dst = os.path.join(os.path.dirname(__file__), 'Dataset/'+row[1])
+                if not (os.path.isdir(dst)):
+                    os.makedirs(dst)
+                src = os.path.join(os.path.dirname(__file__), 'forms/'+row[0]+'.png') 
+                print src          
+                shutil.copy(src, dst)
